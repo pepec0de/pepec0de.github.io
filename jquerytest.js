8be38ed6f2e775556bebd93497b2ac161c7e881c
Pepe González <pepeglezabad@gmail.com>
2020-04-15 15:00:14 +0200

    Update jquerytest.js

--- jquerytest.js
+++ jquerytest.js
@@ -1,3 +1,6 @@
-$(function() {
+/*$(function() {
 $('#loadContent').load('https://raw.githubusercontent.com/pepec0de/WebApp_Sketches/master/chatbox/index.html');
+});*/
+$().ready(function() {
+   $("#text").html("Text added by jQuery code.");
 });
